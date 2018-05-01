b2285c3666bd46fa31e4555eb0fefe5169475e9d
VinuthnaGummadi <vinuthna.gummadi@gmail.com>
2018-05-01 13:54:51 -0500

    commit

---
+++ src/shoppingmall/src/main/java/com/idbms/shoppingmall/util/WebResponse.java
@@ -0,0 +1,27 @@
+package com.idbms.shoppingmall.util;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WebResponse {
+
+	 private boolean success = true;
+	    private String messageCode = "";
+	    private String message = "";
+	    private Object data = null;
+
+	    public WebResponse(boolean success) {
+	        this.success = success;
+	    }
+
+
+	    public WebResponse(boolean success, String message) {
+	        this.success = success;
+	        this.message = message;
+	    }
+}
